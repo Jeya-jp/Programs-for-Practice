7b9adde977666c4f053fdb8152b2f94db97d271a
Jeya-jp <71859343+Jeya-jp@users.noreply.github.com>
2021-03-17 17:24:29 +0530

    Speed Test Completed

---
+++ Speed Test/Speed Test/Speed_Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Speed_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the speed: ");
+            int spd = int.Parse(Console.ReadLine());
+            speed(spd);
+            Console.ReadKey();
+        }
+
+        static void speed(int speed)
+        {
+            if (speed <= 10)
+            {
+                Console.Write("\nSlow");
+            }
+            else if ((speed > 10) && (speed <= 40))
+            {
+                Console.Write("\nAverage");
+            }
+            else if ((speed > 40) && (speed <= 100))
+            {
+                Console.Write("\nFast");
+            }
+            else if ((speed > 100) && (speed <= 180))
+            {
+                Console.Write("\nVery Fast");
+            }
+            else if (speed > 180)
+            {
+                Console.Write("\nExtremely Fast");
+            }
+        }
+
+    }
+}
